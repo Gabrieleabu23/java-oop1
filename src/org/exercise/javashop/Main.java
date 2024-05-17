e41eff7cf6e08711961b825c1d4fbdb302570cbc
Gabrieleabu23 <kaidgabriele23@gmail.com>
2024-05-17 16:03:58 +0200

    add: first package java shop

--- src/org/exercise/javashop/Main.java
+++ src/org/exercise/javashop/Main.java
@@ -1,2 +1,44 @@
-package org.exercise.javashop;public class Main {
+package org.exercise.javashop;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Inserisci il nome del prodotto: ");
+        String nome = scanner.nextLine();
+        System.out.println();
+        System.out.print("Inserisci la descrizione del prodotto del prodotto: ");
+        String descrizione = scanner.nextLine();
+        System.out.println();
+        System.out.print("Inserisci il prezzo del prodotto: ");
+        double price = Double.parseDouble(scanner.nextLine());
+        System.out.println();
+        System.out.print("Inserisci l'iva: ");
+        int vat = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Dettagli del prodotto ********************* ");
+
+        product product = new product(nome, descrizione, price, vat);
+
+        System.out.println("Codice Prodotto: \t" + product);
+        System.out.printf("Prezzo senza iva:\t %.2f € \n", product.getBasePrice());
+        System.out.printf("Prezzo con iva:\t %.2f € \n", product.getFullPrice());
+        System.out.println("Codice-nome del prodotto: \t" + product.getFullName());
+
+//        SETTER
+        product.setCodice(19923);
+        product.setNome("Nuovo Nome");
+        product.setDescrizione("lorem ipsum");
+        product.setPrezzo(150.0);
+        product.setIva(10);
+
+        System.out.println("SETTER-***********************");
+        // Stampiamo le nuove informazioni del prodotto
+        System.out.println("Nuovo nome del prodotto: \t" + product.getFullName());
+        System.out.printf("Nuovo prezzo senza iva:\t %.2f € \n", product.getBasePrice());
+        System.out.printf("Nuovo prezzo con iva:\t %.2f € \n", product.getFullPrice());
+        scanner.close();
+    }
 }
--- src/org/exercise/javashop/product.java
+++ src/org/exercise/javashop/product.java
@@ -1,2 +1,85 @@
-package org.exercise.javashop;public class product {
+package org.exercise.javashop;
+
+import java.util.Random;
+
+public class product {
+
+    Random rand = new Random();
+
+    private int codice = randomCode();
+    private String nome;
+    private String descrizione;
+    private double prezzo = 0.0;
+    private int iva = 0;
+
+    private int error = 0;
+
+    private int randomCode() {
+        return rand.nextInt(120);
+    }
+
+    //CONSTRUCTOR PER IMPOSTARE I DATI TRAMITE NEW NEL MAIN
+    public product(String nome, String descrizione, double prezzo, int iva) {
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = CheckPrice(prezzo);
+        this.iva = iva;
+    }
+
+    private double CheckPrice(double prezzo) {
+        if (prezzo < 0) {
+            System.out.println("Il prezzo non può essere negativo. Impostato a 0.");
+            error++;
+        }
+        return prezzo;
+    }
+
+
+    public double getBasePrice() {
+        return error > 0 ? 0 : prezzo;
+    }
+
+    public double getFullPrice() {
+        return error > 0 ? 0 : (prezzo * (1 + (double) iva / 100));
+    }
+
+    public String getFullName() {
+        return String.format("%06d-%s", codice, nome);
+    }
+
+    // OVERRIDE PER FAR APPARIRE IL CODICE COME STRINGA ALL'INIZIO
+    @Override
+    public String toString() {
+        return String.format("%06d", codice);
+    }
+
+//    GETTER E SETTER
+
+
+    public int setCodice(int codice) {
+        String codiceStringa = String.valueOf(codice);
+
+        if  (codiceStringa.length() <=6){
+            this.codice = codice;
+            return this.codice;
+        }
+        return codice;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public void setIva(int iva) {
+        this.iva = iva;
+    }
+
 }
